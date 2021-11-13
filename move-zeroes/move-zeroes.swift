8612c871c5479ef0de7e0692a8dfc4605a107853
Jiwoong Hwang <hzw94@naver.com>
2021-11-13 14:14:50 +0900

    Time: 168 ms (78.18%), Space: 15.2 MB (28.99%) - LeetHub

--- move-zeroes/move-zeroes.swift
+++ move-zeroes/move-zeroes.swift
@@ -1,22 +1,14 @@
 class Solution {
-    func swap(_ a: Int, _ b: Int, _ nums: inout [Int]) {
-        let t = nums[a]
-        nums[a] = nums[b]
-        nums[b] = t
-    }
-    
     func moveZeroes(_ nums: inout [Int]) {
-        if nums.count <= 1 {
-            return
-        }
-        var last = nums.count - 1
-        for i in stride(from: nums.count - 1, to: -1, by: -1){
-            if nums[i] == 0, i < last {
-                for j in (i + 1)...last {
-                    swap(j, j - 1, &nums)
-                }
-                last -= 1
+        var last = 0
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums[last] = nums[i]
+                last += 1
             }
+        }
+        for i in last..<nums.count {
+            nums[i] = 0
         }
         
     }
