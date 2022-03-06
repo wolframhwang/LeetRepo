7389a5cb35b6bc4adc409c455a36335e71106c53
Jiwoong Hwang <hzw94@naver.com>
2022-03-06 23:40:33 +0900

    Time: 54 ms (16.96%), Space: 14.2 MB (55.56%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count
+        var ans = 0
+        while left < right {
+            let mid = (left + right) / 2
+            if nums[mid] == target { left = mid; break }
+            if nums[mid] < target {
+                left = mid + 1
+            }else {
+                right = mid
+            }
+        }
+        return left
+    }
+}
