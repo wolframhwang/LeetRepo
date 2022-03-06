7962cd2bc1cb0c6ec425f3f91b04827d9ac830ce
Jiwoong Hwang <hzw94@naver.com>
2022-03-06 23:56:54 +0900

    Time: 708 ms (96.05%), Space: 26 MB (85.03%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.swift
+++ 234-palindrome-linked-list/234-palindrome-linked-list.swift
@@ -7,13 +7,16 @@ class Solution {
         
         var fastHead = head
         var slowHead = head
+        var compare = head
+        var now: ListNode? = nil
+        
         while fastHead != nil {
             fastHead = fastHead?.next?.next
             slowHead = slowHead?.next
         }
         
         var node: ListNode?
-        var now = slowHead
+        now = slowHead
         while now != nil {
             let next = now?.next
             now?.next = node
@@ -21,7 +24,6 @@ class Solution {
             now = next
         }
         
-        var compare = head
         while compare != nil, node != nil {
             if compare!.val != node!.val {
                 return false
