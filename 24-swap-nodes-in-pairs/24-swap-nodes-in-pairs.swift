b49d5849bcb04569c685879cc11e9998d4381f83
Jiwoong Hwang <hzw94@naver.com>
2022-03-08 22:03:44 +0900

    Time: 4 ms (81.55%), Space: 14.1 MB (25.97%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.swift
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+*/
+
+
+class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        var node = head
+        guard var nodeNext = head?.next else { return head }
+        let ret = nodeNext
+        
+        var before: ListNode? = nil
+        while true {
+            //print(node?.val, nodeNext.val)
+            let temp = nodeNext.next
+            node?.next = temp
+            nodeNext.next = node
+            if before != nil { before?.next = nodeNext }
+            
+            if temp == nil { return ret }
+            before = node
+            node = temp
+            if node?.next == nil  { return ret }
+            nodeNext = node?.next as! ListNode
+            //print("HELL")
+        }
+        return ret
+    }
+}
