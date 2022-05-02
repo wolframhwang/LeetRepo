ff435d622b360bb6f53b5d03d876441698314cd2
Jiwoong Hwang <hzw94@naver.com>
2022-05-03 00:39:25 +0900

    Time: 36 ms (5.63%), Space: 14.2 MB (74.50%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.swift
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.swift
@@ -1,10 +1,27 @@
 let fail = -1
 class Solution {
+    var nums: [Int] = []
+    func binary(_ l: Int, _ r: Int, _ target: Int) -> Int? {
+        var l = l, r = r
+        while l < r {
+            let m = (l + r) / 2
+            if nums[m] == target {
+                return m
+            }
+            else if nums[m] > target {
+                r = m
+            } else {
+                l = m + 1
+            }
+        }
+        return nil
+    }
     func search(_ nums: [Int], _ target: Int) -> Int {
         // Todo: 2 번 탐색 해야함
         let size = nums.count
         if size == 0 { return fail }
         if size == 1 { return nums[0] == target ? 0 : fail}
+        self.nums = nums
         var answer = -1
         var s = 0, e = size - 1
         while s < e {
@@ -19,30 +36,8 @@ class Solution {
         }
         
         //
-        var l = 0, r = e
-        while l < r {
-            let m = (l + r) / 2
-            if nums[m] == target {
-                return m
-            }
-            else if nums[m] > target {
-                r = m
-            } else {
-                l = m + 1
-            }
-        }
-        
-        l = e; r = size
-        while l < r {
-            let m = (l + r) / 2
-            if nums[m] == target {
-                return m
-            } else if nums[m] > target {
-                r = m
-            } else {
-                l = m + 1
-            }
-        }
+        if let result = binary(0, e, target) { return result }
+        if let result = binary(e, size, target) { return result }
         
         return answer
     }
