bde579b24b2874f8015167ff3d9371b321618a94
Jiwoong Hwang <hzw94@naver.com>
2022-05-04 02:21:49 +0900

    Time: 259 ms (19.15%), Space: 14.4 MB (61.70%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.swift
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.swift
@@ -1,22 +1,45 @@
 class Solution {
+    let INF = 987654321
     func findUnsortedSubarray(_ nums: [Int]) -> Int {
-        //print(nums)
-        var stack: [Int] = []
-        var stack2: [Int] = []
-        var l = nums.count, r = 0
-        for i in 0..<nums.count {
-            while (!stack.isEmpty && nums[stack.last!] > nums[i]) {
-                l = min(l, stack.popLast()!)
+        var startNode = -1
+        for i in 1..<nums.count {
+            if nums[i] < nums[i - 1] {
+                startNode = i - 1
+                break
             }
-            stack.append(i)
+        }
+        if startNode == -1 { return 0 }
+        var endNode = -1
+        for i in (0..<(nums.count - 1)).reversed() {
+            if nums[i] > nums[i + 1] {
+                endNode = i + 1
+                break
+            }
+        }
+        if endNode == -1 { return nums.count }
+        
+        var maxx = -INF, minn = INF
+        for i in startNode...endNode {
+            maxx = max(maxx, nums[i])
+            minn = min(minn, nums[i])
         }
         
-        for i in (0..<nums.count).reversed() {
-            while !stack2.isEmpty, nums[stack2.last!] < nums[i] {
-                r = max(r, stack2.popLast()!)
-            }
-            stack2.append(i)
+        var s = 0, e = startNode
+        var seq = [0, 0]
+        while s < e {
+            let mid = (s + e) / 2
+            if nums[mid] <= minn { s = mid + 1}
+            else { e = mid }
         }
-        return r - l > 0 ? r - l + 1: 0
+        seq[0] = e
+        s = endNode; e = nums.count
+        while s < e {
+            let mid = (s + e) / 2
+            if nums[mid] < maxx { s = mid + 1}
+            else { e = mid }
+        }
+        seq[1] = e
+        
+        return seq[1] - seq[0]
     }
 }
