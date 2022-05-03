5ad5aad6f6d7579f14d78fd777116e61cdddeb4a
Jiwoong Hwang <hzw94@naver.com>
2022-05-04 02:17:07 +0900

    Time: 301 ms (6.38%), Space: 14.6 MB (19.15%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.swift
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.swift
@@ -1,59 +1,21 @@
 class Solution {
-    var nums: [Int] = []
-    let INF = 987654321
-    func upperBound(_ s: Int, _ e: Int, _ target: Int) -> Int {
-        var s = s, e = e
-        
-        while s < e {
-            let mid = (s + e) / 2
-            if nums[mid] <= target { s = mid + 1}
-            else { e = mid }
-        }
-        return e
-    }
-    func lowerBound(_ s: Int, _ e: Int, _ target: Int) -> Int {
-        var s = s, e = e
-        
-        while s < e {
-            let mid = (s + e) / 2
-            if nums[mid] < target { s = mid + 1}
-            else { e = mid }
-        }
-        return e
-    }
-    func findStartNode() -> Int {
-        for i in 1..<nums.count {
-            if nums[i] < nums[i - 1] {
-                return i - 1
-            }
-        }
-        return -1
-    }
-    func findEndNode() -> Int {
-        for i in (0..<(nums.count - 1)).reversed() {
-            if nums[i] > nums[i + 1] {
-                return i + 1
-            }
-        }
-        return -1
-    }
     func findUnsortedSubarray(_ nums: [Int]) -> Int {
         //print(nums)
-        self.nums = nums
-        var startNode = findStartNode()
-        if startNode == -1 { return 0 }
-        var endNode = findEndNode()
-        if endNode == -1 { return nums.count }
-        //print(startNode, endNode, nums)
-        
-        var maxx = -INF, minn = INF
-        for i in startNode...endNode {
-            maxx = max(maxx, nums[i])
-            minn = min(minn, nums[i])
+        var stack: [Int] = []
+        var l = nums.count, r = 0
+        for i in 0..<nums.count {
+            while (!stack.isEmpty && nums[stack.last!] > nums[i]) {
+                l = min(l, stack.popLast()!)
+            }
+            stack.append(i)
         }
-        let s = upperBound(0, startNode, minn)
-        let e = lowerBound(endNode, nums.count, maxx)
-        
-        return e - s
+        stack = []
+        for i in (0..<nums.count).reversed() {
+            while !stack.isEmpty, nums[stack.last!] < nums[i] {
+                r = max(r, stack.popLast()!)
+            }
+            stack.append(i)
+        }
+        return r - l > 0 ? r - l + 1: 0
     }
 }
