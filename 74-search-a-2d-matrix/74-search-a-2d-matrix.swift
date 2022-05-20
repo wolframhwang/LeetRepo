9bc0b771a4fdb2555073979bd4a05983d2d4d363
Jiwoong Hwang <hzw94@naver.com>
2022-05-20 12:33:55 +0900

    Time: 43 ms (8.20%), Space: 14.2 MB (39.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
@@ -1,23 +1,20 @@
 class Solution {
-    func binarySearch(_ matrix: inout [Int], _ target: Int) -> Bool {
-        var s = 0, e = matrix.count
-        while s < e {
-            let m = (s + e) / 2
-            if matrix[m] == target { return true }
-            if matrix[m] < target {
-                s = m + 1
-            } else {
-                e = m
-            }
-        }
-        return false
-    }
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        var matrix = matrix
+        let N = matrix[0].count
+        
+        var s = 0, e = 0
         for i in 0..<matrix.count {
-            if binarySearch(&matrix[i], target) {
-                return true
-            }
+            s = 0; e = N
+            
+            while s < e {
+                let m = (s + e)  / 2
+                if matrix[i][m] == target { return true }
+                if matrix[i][m] < target {
+                    s = m + 1
+                } else {
+                    e = m
+                }
+            }                        
         }
         return false        
     }
