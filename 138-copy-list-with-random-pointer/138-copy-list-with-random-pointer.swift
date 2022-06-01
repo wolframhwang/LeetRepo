87d5cb319bf0944476811881f95e7e0c4560840b
Jiwoong Hwang <hzw94@naver.com>
2022-06-01 21:37:43 +0900

    Time: 34 ms (33.18%), Space: 15.5 MB (32.74%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.swift
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.swift
@@ -1,48 +1,26 @@
-/**
- * Definition for a Node.
- * public class Node {
- *     public var val: Int
- *     public var next: Node?
- *     public var random: Node?
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.next = nil
- *    	   self.random = nil
- *     }
- * }
- */
-
 class Solution {
     func copyRandomList(_ head: Node?) -> Node? {
-        if head == nil { return nil }
-        //let ret: Node? = Node(head?.val ?? 0)
-        var node = head
-        var origin: [Node?] = []
-        var nodeArray: [Node?] = []
-        
-        while node != nil {
-            guard let value = node?.val else { return node }
-            origin.append(node)
-            let inp: Node? = Node(value)
-            if nodeArray.count > 0 {
-                nodeArray[nodeArray.count - 1]?.next = inp
-            }
-            nodeArray.append(inp)
-            node = node?.next
+        if head == nil { 
+            return nil 
         }
-        
-        for i in 0..<origin.count {
-            let now = origin[i]?.random
+        var map: [Node : Node] = [:]
+        var old = head
+        var current:Node? = Node(0)
+
+        while old != nil {
+            current?.val = old!.val
+            map[old!] = current
+            if old?.random != nil {
+                current?.random = map[old!.random!] ?? Node (old!.random!.val)
+                map[old!.random!] = current?.random
+            }
+            old = old?.next
+            if old != nil {
+                current?.next = map[old!] ?? Node(0)
+                current = current?.next
+            }
             
-            for j in 0..<origin.count {
-                if now === origin[j] {
-                    nodeArray[i]?.random = nodeArray[j]
-                    break
-                }
-            }
         }
-        
-        
-        return nodeArray[0]
+        return map[head ?? Node(0)]
     }
 }
